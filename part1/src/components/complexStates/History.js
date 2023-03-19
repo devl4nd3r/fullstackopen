882704bca7480166873df3432837a7b8a7314682
devl4nd3r <dev.l4nd3r@gmail.com>
2023-03-19 16:07:42 -0500

    removed part1 older folder

--- part1/src/components/complexStates/History.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const History = ({ clicks }) => {
-  if (clicks.length === 0) {
-    return <p>Historial con valor inicial 0</p>
-  }
-
-  return <p>{clicks.join(" ")}</p>
-}
--- part1/src/components/liftingStateUp/ButtonCounter.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const ButtonCounter = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>
-}
